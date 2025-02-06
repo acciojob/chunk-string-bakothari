cdcba09a43aeeedb41a699fea0de33f7a0083635
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-06 12:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function stringChop(str, size) {
   // your code here
-	if(str=="")
-		return []
      let result =[]
-	for(let i =0;i<str.length;i+=size){
-	if (i + size <= str.length) {
-      result.push(str.slice(i, i + size));
-    } else {
-      result.push(str.slice(i));
-    }
-		return result
-}
+	if (str === "") return [];
+
+  let result = [];
+  for (let i = 0; i < str.length; i ++) {
+    result.push(str.slice(i, i + size));
+  }
+  return result;
+} 
 
 // Do not change the code below
 const str = prompt("Enter String.");
